c05b62f5ed2ad1ca8836a0c0107a52a26d33fd1a
pankajw-cuelogic <pankaj.wakchaure@cuelogic.co.in>
2016-04-04 17:04:57 +0530

    master : Day1Test : 4 April 2016 : Demo app to concat first name and last name with validations

---
+++ Day1Test/Day1Test/app/controller/personController.js
@@ -0,0 +1,11 @@
+﻿app.controller('personCtrl', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Jack";
+    $scope.fullName = function () {
+        var message = '';
+        if ($scope.firstName == undefined || $scope.lastName == undefined) {
+            return "First Name or Last Name is Empty";
+        }
+        return "Full Name : " + $scope.firstName + " " + $scope.lastName;
+    };
+});
