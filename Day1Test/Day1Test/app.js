889ea5ea7c2e9f2416596986d6d6ad9ba641e81e
pankajw-cuelogic <pankaj.wakchaure@cuelogic.co.in>
2016-04-06 19:33:31 +0530

    Day2_communicate_controllers_directly : code push for intercommunication of controllers

--- Day1Test/Day1Test/app.js
+++ Day1Test/Day1Test/app.js
@@ -1,14 +1,16 @@
-﻿var demoApp = angular.module('demoApp', ['ui.router']);
-demoApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/parent");
-    $stateProvider.state('parent', {
-        url: '/parent',
-        templateUrl: 'app/views/nestedviews/parent.html',
-        controller: 'parentController'
+﻿//var demoApp = angular.module('demoApp', ['ui.router']);
+//demoApp.config(function ($stateProvider, $urlRouterProvider) {
+//    $urlRouterProvider.otherwise("/parent");
+//    $stateProvider.state('parent', {
+//        url: '/parent',
+//        templateUrl: 'app/views/nestedviews/parent.html',
+//        controller: 'parentController'
 
-    }).state('parent.child', {
-        url: '/child',
-        templateUrl: 'app/views/nestedviews/child.html',
-        controller: 'childController'
-    });
-});
+//    }).state('parent.child', {
+//        url: '/child',
+//        templateUrl: 'app/views/nestedviews/child.html',
+//        controller: 'childController'
+//    });
+//});
+
+
---
+++ Day1Test/Day1Test/app/scripts/controller/twoController.js
@@ -0,0 +1,6 @@
+﻿
+myModule.controller('twoController', function ($scope, sharedService) {
+    $scope.$on('handleBroadcast', function () {
+        $scope.message = 'Data from second controller: ' + sharedService.message;
+    });
+});
